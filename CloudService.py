9f418dc7fc7d3672890b9f7830497857afe1c09f
Nick <nshokri123@gmail.com>
2020-12-17 14:37:05 -0800

    Added translate

--- CloudService.py
+++ CloudService.py
@@ -1,6 +1,8 @@
-import requests
 from configparser import ConfigParser
 from google.cloud import speech
+import six
+from google.cloud import translate_v2 as translate
+
 import io
 
 class Cloud:
@@ -29,6 +31,7 @@ class Cloud:
         config = speech.RecognitionConfig(
             encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
             sample_rate_hertz=16000,
+            audio_channel_count=2,
             language_code=language_code
         )
 
@@ -49,3 +52,24 @@ class Cloud:
             print("Confidence: {}".format(result.alternatives[0].confidence))
         
         return complete_text
+
+    def translate(self, text: str, source_language: str, target_language: str) -> str:
+        """Translates text into the target language.
+
+        Target must be an ISO 639-1 language code.
+        See https://g.co/cloud/translate/v2/translate-reference#supported_languages
+        """
+
+        translate_client = translate.Client()
+
+        if isinstance(text, six.binary_type):
+            text = text.decode("utf-8")
+
+        # Text can also be a sequence of strings, in which case this method
+        # will return a sequence of results for each text.
+        result = translate_client.translate(text, source_language=source_language,
+                                            target_language=target_language)
+
+        print(u"Text: {}".format(result["input"]))
+        print(u"Translation: {}".format(result["translatedText"]))
+        print(u"Detected source language: {}".format(result["detectedSourceLanguage"]))
