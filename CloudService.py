fd5ba8da9725ec72ccf132177e01b7699bd006bb
Nick <nshokri123@gmail.com>
2020-12-21 16:13:00 -0800

    Added translation

--- CloudService.py
+++ CloudService.py
@@ -1,26 +1,19 @@
-from configparser import ConfigParser
 import six
 
 from google.cloud import speech
 from google.cloud import translate_v2 as translate
 from google.cloud import storage
 
-from google.cloud import speech_v1p1beta1
+from google.cloud import speech
 
-import io
 
 class Cloud:
 
-    def __init__(self):
-        # Read in api key from config file
-        #config = ConfigParser()
-        #config.read('config.ini')
+    def __init__(self, in_language, out_language):
+        self.in_language = in_language
+        self.out_language = out_language
 
-        #self.api_key = config['account']['api_key']
-        pass
-        #print(self.api_key)
-
-    def audio_to_text(self, speech_file, language_code: str) -> str:
+    def audio_to_text(self, speech_file):
         """Transcribe the given audio file asynchronously."""
         client = speech.SpeechClient()
 
@@ -38,10 +31,9 @@ class Cloud:
         audio = speech.RecognitionAudio(uri='gs://async_audio_files/' + speech_file)
 
         config = speech.RecognitionConfig(
-            #encoding=speech.RecognitionConfig.AudioEncoding.AMR_WB,
-            sample_rate_hertz=16000,
-            audio_channel_count=2,
-            language_code=language_code,
+            # encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
+            # sample_rate_hertz = 44100,
+            language_code=self.in_language,
             enable_word_time_offsets=True,
             enable_automatic_punctuation=True
         )
@@ -49,34 +41,33 @@ class Cloud:
         operation = client.long_running_recognize(config=config, audio=audio)
 
         print("Waiting for operation to complete...")
-        response = operation.result(timeout=90)
+        response = operation.result(timeout=300)
 
-        complete_text = []
+        srt_file = open(speech_file.split(".")[0] + ".srt", "w+")
+
+        sequence = 1
 
         for result in response.results:
             alternative = result.alternatives[0]
-            print("Transcript: {}".format(alternative.transcript))
-            print("Confidence: {}".format(alternative.confidence))
+            start_time = str(alternative.words[0].start_time)
+            end_time = str(alternative.words[-1].end_time)
 
-            for word_info in alternative.words:
-                word = word_info.word
-                start_time = word_info.start_time
-                end_time = word_info.end_time
+            srt_file.write(str(sequence) + "\n")
+            srt_file.write(self.format_time_stamp(start_time) + " --> " + self.format_time_stamp(end_time) + "\n")
+            srt_file.write(translate(alternative.transcript) + "\n" + "\n")
+            sequence += 1
 
-                complete_text.append({'word': word, 'start': start_time, 'end': end_time})
+        srt_file.close()
+        self.delete_blob('async_audio_files', speech_file)
+        print("Done Transcribing .SRT")
 
-                print(
-                    f"Word: {word}, start_time: {start_time.total_seconds()}, end_time: {end_time.total_seconds()}"
-                )
-        
-        return complete_text
-
-    def translate(self, text: str, source_language: str, target_language: str) -> str:
+    def translate(self, text: str) -> str:
         """Translates text into the target language.
-
         Target must be an ISO 639-1 language code.
         See https://g.co/cloud/translate/v2/translate-reference#supported_languages
         """
+        if (self.in_language == self.out_language):
+            return text
 
         translate_client = translate.Client()
 
@@ -85,12 +76,14 @@ class Cloud:
 
         # Text can also be a sequence of strings, in which case this method
         # will return a sequence of results for each text.
-        result = translate_client.translate(text, source_language=source_language,
-                                            target_language=target_language)
+        result = translate_client.translate(text, source_language=self.in_language,
+                                            target_language=self.out_language)
+
 
         print(u"Text: {}".format(result["input"]))
         print(u"Translation: {}".format(result["translatedText"]))
         print(u"Detected source language: {}".format(result["detectedSourceLanguage"]))
+
 
     def upload(self, bucket_name, source_file_name, destination_name):
         """Uploads a file to the bucket."""
@@ -109,3 +102,13 @@ class Cloud:
         blob = bucket.blob(blob_name)
 
         blob.delete()
+
+    def format_time_stamp(self, time_stamp):
+        time_stamp = "0" + time_stamp
+        if (len(time_stamp) == 8):
+            time_stamp += ",000"
+        else:
+            time_stamp = time_stamp.replace(".", ",")
+            time_stamp = time_stamp[0:12]
+
+        return time_stamp
